c8efc46152cbe58f787d9f5889242875fbb2fddb
hugojesusmr <hugojesusmr@gmail.com>
2021-04-28 11:10:25 -0500

    Spring Boot Security Form Authentication with In-memory Users

--- src/main/java/com/javacode/springbootsecurity/basic/LoginController.java
+++ src/main/java/com/javacode/springbootsecurity/basic/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class LoginController {
     
-    @GetMapping("/")
-    public String hello(){
-        return "index";
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 }
--- src/main/java/com/javacode/springbootsecurity/basic/WebSecurityConfig.java
+++ src/main/java/com/javacode/springbootsecurity/basic/WebSecurityConfig.java
@@ -1,39 +1,48 @@
 package com.javacode.springbootsecurity.basic;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails. UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-        .passwordEncoder(new BCryptPasswordEncoder())
-            .withUser("hugo")
-            .password("$2a$10$iOwoM2EAn64sOL9FTAe5wuYCOYXt5qTX6oFcw.ZElhBJFxSaalMne")
-            .roles("USER") 
-            
-        .and()
-            .withUser("jesus")
-            .password("$2a$10$xos.gKEt7n5Ql6TDYX296eIqnGqWJIbsyrqsbBc6b.kKkHk.OS3Jq")
-            .roles("ADMIN"); 
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        .authorizeRequests()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .httpBasic();
-
-        
-    }
+    
+   @Override
+   @Bean
+   protected UserDetailsService userDetailsService() {
+       UserDetails userDetails =  User
+                     .withUsername("HUGO")
+                     .password("$2a$10$eCMafrH2H2kquDbVX8lGaeMD6yrFjPmNmqRC/G5z.QbTLH9VEBqIK")
+                     .roles("USER")
+                     .build();
+       return new InMemoryUserDetailsManager(userDetails);
+   }
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+       http
+            .authorizeRequests().antMatchers("/").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+            .logout()
+            .permitAll();    
+   }
 }
